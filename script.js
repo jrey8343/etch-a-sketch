fc65fc466d671fa12eb0f864d6a18d05598020e2
Jared Reyes <jaredreyespt@gmail.com>
2023-05-06 12:25:32 +1000

    slider and button working

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
+const root = document.querySelector(':root');
 const wrapper = document.querySelector('.wrapper');
 const button = document.querySelector('.container button');
-const root = document.querySelector(':root');
+const range = document.getElementById('range');
+const readOut = document.querySelector('.readOut');
+let sides = 4;
 
-createBoxes(6);
+createBoxes(sides);
 
+range.addEventListener('input', (e) => {
+ sides = e.target.value;
+ readOut.textContent = `${sides} squares per side`;
+});
 
-function createBoxes(sides) {
+button.addEventListener('click', () => {
+  resetCurrentBoxes();
+  createBoxes(sides);
+
+});
+
+function createBoxes(n) {
   //Calculate the amount of boxes
-  const boxes = sides ** 2;
+  const boxes = n ** 2;
   //Create 16 squares within the container div
   for (let i = 0; i < boxes ; i++) {
     const div = document.createElement('div');
     div.classList.add('box');
     wrapper.appendChild(div);
   };
-  setGridRepeat(sides);
+  setGridRepeat(n);
 };
 
 function setGridRepeat(n) {
   root.style.setProperty('--grid-repeat', n)
 };
+
+function resetCurrentBoxes() {
+  const currentBoxes = document.querySelectorAll('.box');
+  currentBoxes.forEach(currentBox => {
+    wrapper.removeChild(currentBox);
+  });
+};
+
 
 
 
