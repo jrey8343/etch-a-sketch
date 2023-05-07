c5776c6267a25002f125709d3689ac55fb9cec71
Jared Reyes <jaredreyespt@gmail.com>
2023-05-07 17:03:19 +1000

    interface complete

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ const wrapper = document.querySelector('.wrapper');
 const button = document.querySelector('.container button');
 const range = document.getElementById('range');
 const readOut = document.querySelector('.readOut');
-let sides = 4;
+const currentBoxes = document.querySelectorAll('.box');
+let sides = 8;
 
 createBoxes(sides);
 
 range.addEventListener('input', (e) => {
  sides = e.target.value;
- readOut.textContent = `${sides} squares per side`;
+ readOut.textContent = `Generate ${sides} squares per side`;
 });
 
 button.addEventListener('click', () => {
   resetCurrentBoxes();
   createBoxes(sides);
-
 });
+
+wrapper.addEventListener('mouseover', (e) => {
+  if(e.target.className === 'box') {
+    e.target.style.backgroundColor = '#877087';
+  }
+})
 
 function createBoxes(n) {
   //Calculate the amount of boxes
@@ -35,7 +41,6 @@ function setGridRepeat(n) {
 };
 
 function resetCurrentBoxes() {
-  const currentBoxes = document.querySelectorAll('.box');
   currentBoxes.forEach(currentBox => {
     wrapper.removeChild(currentBox);
   });
